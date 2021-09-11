d34adf857e5d52085d1138b1f99228996489acdf
naiggy <51503355+naiggy@users.noreply.github.com>
2021-09-11 16:38:05 +0900

    Initial commit
    Private SSL Certificate on Apache

---
+++ run.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+echo "Private SSL Certificate on Apache"
+echo "This is for local testing only and does not take any responsibility"
+echo "Expired 2031/09/09"
+echo "Do you want to install it?"
+echo "1) Yes"
+echo "2) No"
+read -p "Please, select number 1 or 2 : " yn
+case "$yn" in
+    1) echo "Install it. .";;
+    *) echo "Abort the  installation. bye ";exit ;;
+esac
+
+dnf install -y mod_ssl
+cd private_ssl
+chmod 400 localhost*
+chmod 755 password
+
+file_exetension=_`date "+%Y%m%d_%H%M%S"`
+
+if [ -e /etc/pki/tls/private/localhost.key ];then
+    mv /etc/pki/tls/private/localhost.key /etc/pki/tls/private/localhost.key${file_exetension}
+fi
+if [ -e /etc/pki/tls/certs/localhost.crt ];then
+    mv /etc/pki/tls/certs/localhost.crt /etc/pki/tls/certs/localhost.crt${file_exetension}
+fi
+if [ -e /etc/pki/tls/certs/localhost.csr ];then
+    mv /etc/pki/tls/certs/localhost.csr /etc/pki/tls/certs/localhost.csr${file_exetension}
+fi
+cp -p ./localhost.key /etc/pki/tls/private/
+cp -p ./localhost.crt /etc/pki/tls/certs/
+cp -p ./localhost.csr /etc/pki/tls/certs/
+cp -p ./password /usr/libexec/
+
+mv /etc/crypto-policies/config /etc/crypto-policies/config${file_exetension}
+echo "LEGACY" > /etc/crypto-policies/config
+update-crypto-policies
+cp -p /etc/httpd/conf.d/ssl.conf /etc/httpd/conf.d/ssl.conf${file_exetension}
+sed -i -e 's/SSLPassPhraseDialog exec:\/usr\/libexec\/httpd-ssl-pass-dialog/SSLPassPhraseDialog exec:\/usr\/libexec\/password/g' /etc/httpd/conf.d/ssl.conf
+
+systemctl restart httpd
+
+echo "Product by park.iggy"
+echo "Email adress naiggy@gmail.com"
